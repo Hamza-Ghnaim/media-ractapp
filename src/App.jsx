6d922bfb146aedeac315c32f7d5b9f663c1cdf44
Hamza <hamzaghnaim99@gmail.com>
2023-02-15 17:54:46 -0500

    implemented basic protection

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState} from 'react'
 import {BrowserRouter,Route,Routes} from "react-router-dom";
 import Login from './Login/Login';
 import Albums from './Albums/Albums';
 import Context from './ContextFolder/Context';
 import Photos from './Albums/Photos';
+import ProtectedRoutes from './SecuredRoutes/ProtectedRoutes';
 import './App.css'
 
 
 
 function App() {
   const [userInfo, setUserInfo] = useState(JSON.parse(localStorage.userinfo));
+
   // const [userInfo, setUserInfo] = useState("");
   // useEffect(()=>{
   //   try{
@@ -26,8 +28,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path ="/" element={<Login/>} />
-          <Route path ="/Albums" element={<Albums/>} />
-          <Route path ="/Albums/Photos" element={<Photos/>} />
+          <Route element={<ProtectedRoutes/>}>
+            <Route path ="/Albums" element={<Albums/>} />
+            <Route path ="/Albums/Photos" element={<Photos/>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </Context.Provider>
